a4a8db764a519fece31aab2d83a8964969753e47
FelipeHardmann <fashardmann@gmail.com>
2022-12-14 00:37:59 -0300

    Alterei tudo

--- config/connection.py
+++ config/connection.py
@@ -1,19 +1,57 @@
-import mysql.connector
-import os
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker
+)
+from datetime import datetime as dt
+
+# Conectando ao banco
+engine = create_engine(
+    url = f'mysql+mysqlconnector://felipe:12345@localhost:3306/copa', 
+    echo = True
+)
+
+Base = declarative_base()
+
+class Tecnico(Base):
+    __tablename__: str = 'tecnico'
+
+    id_tecnico: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    nome_tecnico: str = sa.Column(sa.String(30), nullable=False)
+
+    def __repr__(self) -> str:
+        '''Essa função retorna a representação do objeto'''
+        return f''
 
 
-class Connection:
+class Arbitro(Base):
+    __tablename__: str = 'arbitro'
 
-    def connect(self, path_envfile):
-        try:
-            conn = mysql.connector.connect(
-                host=os.environ['HOST'],
-                port=os.environ['PORT'],
-                user=os.environ['USER'],
-                passwd=os.environ['PASSWD'],
-                db=os.environ['DB']
-            )
-        except Exception as err:
-            raise err('Something went wrong')
-        else:
-            return conn
+    id_arbitro: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    nome_arbitro: str = sa.Column(sa.String(30), nullable=False)
+
+    def __repr__(self) -> str:
+        '''Essa função retorna a representação do objeto'''
+        return f''
+
+
+
+class Fases(Base):
+    __tablename__: str = 'fases'
+
+    id_fase: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    nome_fase: str = sa.Column(sa.String(30), nullable=False)
+
+
+class Grupos(Base):
+    __tablename__: str = 'grupos'
+
+    id_grupo: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    nome_grupo: str = sa.Column(sa.String(20), nullable=False)
+
+    def __repr__(self) -> str:
+        '''Essa função retorna a representação do objeto'''
+        return f''
+
+
